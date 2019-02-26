a4a1f154369e1b2fdf54be22250b62e5a8a7e14f
dennisz <dennis.vanzundert@ns.nl>
2019-02-26 14:16:58 +0100

    first commit

---
+++ src/test/java/GlueCode/GeneralStepDefinitions.java
@@ -0,0 +1,13 @@
+package GlueCode;
+
+import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.PendingException;
+
+public class GeneralStepDefinitions {
+
+    @Given("^I succesfully login to Facebook$")
+    public void i_succesfully_login_to_Facebook() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
---
+++ src/test/resources/Feature_Files/Facebook/Facebook_login.feature
@@ -0,0 +1,5 @@
+Feature: Login to Facebook
+
+  Scenario: Succesfull login to Facebook
+
+    Given I succesfully login to Facebook
